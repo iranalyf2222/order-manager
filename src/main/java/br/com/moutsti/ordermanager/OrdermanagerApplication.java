853e705a15d99fb54f341c55169828bf8702e843
iranalyf <iranalyf2222@gmail.com>
2024-11-28 19:05:21 -0300

    innitial commit

---
+++ src/main/java/br/com/moutsti/ordermanager/OrdermanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.moutsti.ordermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermanagerApplication.class, args);
+	}
+
+}
